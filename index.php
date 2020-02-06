ca4ce511067166700e87e606f90cbc00f2cfd534
Higor Konig <higor.konig@gmail.com>
2020-02-05 23:17:09 -0300

    first commit

--- index.php
+++ index.php
@@ -3,45 +3,51 @@
 
 <head>
   <meta charset="UTF-8">
-  <link rel="shortcut icon" href="img/icon.png" type="image/x-icon">
-  <link rel="stylesheet" href="css/style.css">
-  <title>Login - Mokey Book</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="css/hacker.css">
+  <title>Desafio</title>
 </head>
 
 <body>
+
+
   <div class="container">
-    <form action="php/login.php" method="POST" class="login">
-      <img src="img/login.png" class="img">
-      <input class="campo" type="email" name="email" placeholder="Email" required>
-      <input class="campo" type="password" name="senha" placeholder="Senha" required>
-      <button class="btn" type="submit">Entrar</button>
-      <?php
-      if (isset($_GET['e'])) {
-        $erro = $_GET['e'];
-        if ($erro == "incorreto") {
-          echo "<div class='error'>
-                  <span>Login e/ou senha incorreto(s)!</span>
-              </div>";
-        } else if ($erro == "nao_existe") {
-          echo "<div class='error'>
-                  <span>Usuário não existe</span>
-              </div>";
-        } else {
-          echo "";
-        }
-      }
+    <div class="col-md-4">
+
+    </div>
+    <div class="col-md-4" style="margin-top: 20%;">
+    <h1 class="mb-2">Login </h1>
+      <form>
+        <div class="form-group">
+          <label>E-mail</label>
+          <input type="email" class="form-control"name="email" placeholder="E-mail">
+        </div>
+        <div class="form-group">
+          <label >Senha</label>
+          <input type="password" class="form-control" name="senha" placeholder="Senha">
+        </div>
+        <button type="submit" class="btn btn-primary">Entrar</button>
+      </form>
+
+      <?php 
+      
+    if(isset($_GET['senha'])) {
+      echo $_GET['senha'];
+    }else {
+      
+    }
+      
       ?>
+    </div>
+    <div class="col-md-4">
 
-      <a href="cadastro.php" class="esqueciSenha">Cadastre-se</a><br>
-      <a href="" class="esqueciSenha">Esqueceu a senha?</a>
-
-    </form>
+    </div>
   </div>
 
-  <footer class="footer">
-    <p>Desenvolvido por <a target="_blank" href="https://www.instagram.com/higor.konig/" style="text-decoration: none;">@higor.konig</a></p>
-    <p>GitHub <a target="_blank" href="https://www.github.com/higorkonig/" style="text-decoration: none;">higorkonig</a></p>
-  </footer>
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 </body>
 
 </html>
--- php/sair.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-if (isset($_SESSION['token_acesso'])) {
-    unset($_SESSION['token_acesso']);
-}
-header("Location: ../");
-exit;
